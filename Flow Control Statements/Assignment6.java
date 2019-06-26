7ee2507a95bbd40f01203faad6afab0a7b60b209
kate_99 <46323381+karthik40370@users.noreply.github.com>
2019-06-27 02:11:09 +0530

    Add files via upload

---
+++ Flow Control Statements/Assignment6.java
@@ -0,0 +1,35 @@
+/* Write a program to accept gender ("Male" or "Female") and age from command line arguments and print the percentage of interest based on the given conditions.
+
+If the gender is 'Female' and age is between 1 and 58, the percentage of interest is 8.2%.
+
+If the gender is 'Female' and age is between 59 and 100, the percentage of interest is 9.2%.
+
+If the gender is 'Male' and age is between 1 and 58, the percentage of interest is 8.4%.
+
+If the gender is 'Male' and age is between 59 and 100, the percentage of interest is 10.5%.*/
+
+
+
+public class Assignment6{
+	
+    public static void main(String[] args) {
+    		
+    		String gender = args[0];
+    		int age = Integer.parseInt(args[1]);
+    		
+    		if(gender.equals("Male") || gender.equals("male")) {
+    			if(age>=1 && age<=58)
+    				System.out.println("Interest == 8.4%");
+                else if(age >= 59 && age <= 100)
+                    System.out.println("Interest == 10.5%");
+    			
+    		}
+    		if(gender.equals("Female") || gender.equals("female")) {
+    			if(age>=1 && age<=58)
+    				System.out.println("Interest == 8.2%");
+                else if(age >= 59 && age <= 100)
+                    System.out.println("Interest == 9.2%");
+    		}
+    		
+    	}
+    }
