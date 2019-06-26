122bb83e6b95df44e7fb76385f59d552612f5071
kate_99 <46323381+karthik40370@users.noreply.github.com>
2019-06-27 02:53:31 +0530

    Add files via upload

---
+++ Flow Control Statements/Assignment7.java
@@ -0,0 +1,15 @@
+/*Initialize a character variable with an alphabhet in a program.
+
+If the character value is in lowercase, the output should be displayed in uppercase in the following format.
+
+Example1)
+i/p:a
+o/p:a->A
+
+If the character value is in uppercase, the output should be displayed in lowercase in the following format.
+
+Example2)
+i/p:A
+o/p:A->a*/
+
+Assignment7
